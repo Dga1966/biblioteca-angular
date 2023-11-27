45f40d4b34e0f97f625443cbcb9f45e151abf22c
Daniel Arriaga <danielgerman007@yahoo.com.ar>
2023-11-27 19:33:54 -0300

    Creacion de Tabla dinamicamente

--- src/app/biblioteca-angular/biblioteca-angular.component.ts
+++ src/app/biblioteca-angular/biblioteca-angular.component.ts
@@ -1,12 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-biblioteca-angular',
+  selector: 'biblioteca-angular',
   templateUrl: './biblioteca-angular.component.html',
   styleUrls: ['./biblioteca-angular.component.scss']
 })
 export class BibliotecaAngularComponent implements OnInit {
-
+libros= [
+  {
+      "id": 1,
+      "titulo": "titluo1",
+      "paginas": 12,
+      "autor": null
+  },
+  {
+      "id": 2,
+      "titulo": "titluoActualizado2",
+      "paginas": 88,
+      "autor": {
+          "id": 1,
+          "nombre": "Vargas",
+          "mail": "vargas@gmail",
+          "activo": true
+      }
+  },
+  {
+    "id": 3,
+    "titulo": "titluoActualizado3",
+    "paginas": 89,
+    "autor": {
+        "id": 2,
+        "nombre": "Varguita",
+        "mail": "vargas@",
+        "activo": true
+    }
+}
+];
   constructor() { }
 
   ngOnInit(): void {
